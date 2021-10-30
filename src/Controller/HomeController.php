1a0a10dbeeecfb6f37185bf7d80cb032e09154bc
Radoslaw Niestroj <radoslaw.niestroj2000@gmail.com>
2021-10-30 23:27:34 +0200

    T-1 base functionalities

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @param ProductRepository $productRepository
+     * @return Response
+     */
+    #[Route('/', name: 'home')]
+    public function index(ProductRepository $productRepository): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'products' => $productRepository->findAll(),
+        ]);
+    }
+}
